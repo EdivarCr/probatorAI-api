68f0cbe68f48f133246fa086f0822d8561b3c84e
EdivarCr <edivarfilho16@gmail.com>
2026-04-30 19:41:04 -0300

    fix: ajuste no schema do vercel.json e contratos de auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AppController } from './app.controler';
 
 @Module({
   imports: [AuthModule, UserModule],
+  controllers: [AppController],
 })
 export class AppModule {}
