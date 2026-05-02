9bd3d2d94755ac90e0a9b5f1ccdf90618a525b02
EdivarCr <edivarfilho16@gmail.com>
2026-05-02 18:59:42 -0300

    chore(app): adiciona condig global e primsaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controler';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
