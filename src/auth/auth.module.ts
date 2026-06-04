54ccda838f78e03ce2656f20acdbe7ab16185178
Eduardo-Lima-Dev <limadudu34@gmail.com>
2026-06-04 00:20:13 -0300

    feat(auth): fix JWT config to load secret dynamically from config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { PassportModule } from '@nestjs/passport';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './roles.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -26,10 +26,13 @@ import { RolesGuard } from './roles.guard';
   imports: [
     PassportModule,
     UserModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      inject: [ConfigService],
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: jwtConstants.expiresIn },
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
 })
